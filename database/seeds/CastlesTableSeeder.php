25788c7be4b7e9335bbd772a7c55045a401e751d
Роман Руднев <ryrudnev@gmail.com>
2015-11-11 02:32:01 +0300

    Добавлен фейкер для добавления рандомных записей в таблицу Замко в БД.

--- database/seeds/CastlesTableSeeder.php
+++ database/seeds/CastlesTableSeeder.php
@@ -10,11 +10,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Castle;
+use Faker\Factory;
 
 class CastlesTableSeeder extends Seeder
 {
     public function run()
     {
+        $faker = Factory::create();
 
+        foreach(range(1, 10) as $i) {
+            Castle::create(['name' => $faker->colorName]);
+        }
     }
 }
