d38fef3ecf6a2b0ee01869d3c13fe9df1f529d19
Lex Mosolov <lex.mosolov@gmail.com>
2015-12-21 21:06:35 +0300

    News model created

---
+++ app/Models/News.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Models;
+
+use SleepingOwl\Models\Interfaces\ModelWithImageFieldsInterface;
+use SleepingOwl\Models\SleepingOwlModel;
+use SleepingOwl\Models\Traits\ModelWithImageOrFileFieldsTrait;
+
+class News extends SleepingOwlModel implements ModelWithImageFieldsInterface
+{
+    use ModelWithImageOrFileFieldsTrait;
+
+    protected $table = 'news';
+    protected $fillable = [
+        'title',
+        'date',
+        'published',
+        'text',
+        'photo',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function getImageFields()
+    {
+        return [
+            'photo' => 'news/'
+        ];
+    }
+
+    public function scopeLast($query)
+    {
+        $query->orderBy('date', 'desc')->limit(4);
+    }
+}
