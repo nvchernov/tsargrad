e8de421caf3e7ced4dfde252e46e911ad8ba5379
gvterechov <grvlter@gmail.com>
2015-12-22 22:59:55 +0300

    Добавлены роуты для главной страницы и страниц авторизации

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,9 +13,47 @@
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+// General page
+Route::get('home', [
+    'middleware' => 'auth',
+    'uses' => 'HomeController@home'
+]);
+
+Route::get('user/profile', [
+    'middleware' => 'auth',
+    'uses' => 'UserController@getProfile'
+]);
+Route::post('user/update', [
+    'middleware' => 'auth',
+    'uses' => 'UserController@postUpdate'
+]);
+
+// General page
+Route::get('home', [
+    'middleware' => 'auth',
+    'uses' => 'HomeController@home'
+]);
+
+Route::get('user/profile', [
+    'middleware' => 'auth',
+    'uses' => 'UserController@getProfile'
+]);
+Route::post('user/update', [
+    'middleware' => 'auth',
+    'uses' => 'UserController@postUpdate'
+]);
 
 Route::resource('news', 'NewsController');
 
