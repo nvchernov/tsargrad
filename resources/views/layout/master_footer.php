b47c797eaec00fa53ba7e29528bdfe6c3056b108
gvterechov <grvlter@gmail.com>
2015-12-22 23:23:55 +0300

    Добавлена верстка футера

---
+++ resources/views/layout/master_footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikolay
+ * Date: 11.09.2015
+ * Time: 15:34
+ */
+?>
+<footer class="navbar-fixed-bottom" role="contentinfo">
+    <div class="container">
+        <hr>
+        <p class="text-center" style="color:white;">
+            Tsargrag 2016 (c) VSTU PRIN-2n
+        </p>
+    </div>
+</footer>
+</body>
+</html>
