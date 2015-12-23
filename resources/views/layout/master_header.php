3d0411207b5c55f3d5ec127b5b7a75c6e29bd36a
Роман Руднев <ryrudnev@gmail.com>
2015-12-24 00:31:18 +0300

    Фикс хидера.

--- resources/views/layout/master_header.php
+++ resources/views/layout/master_header.php
@@ -23,7 +23,7 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    /head>
+</head>
 <body>
 
 <header>
